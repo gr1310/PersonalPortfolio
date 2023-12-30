be4f72a452f82f3c6af090c6d693591dddd29238
Garima R <121031637+gr1310@users.noreply.github.com>
2023-12-30 16:27:29 +0530

    Add files via upload

---
+++ Portfolio Website/assets/contact.js
@@ -0,0 +1,10 @@
+$("div.card#card1").hover(function () {
+    $("div.card#card1").removeClass("opacity-50");
+}, function () {
+    $("div.card#card1").addClass("opacity-50");
+});
+$("div.card#card2").hover(function () {
+    $("div.card#card2").removeClass("opacity-50");
+}, function () {
+    $("div.card#card2").addClass("opacity-50");
+});
